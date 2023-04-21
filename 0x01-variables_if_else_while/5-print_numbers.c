0553da505737bf09084e1cd96b59e02a65dfa702
BraveStarShalom <jesway1030@gmail.com>
2023-04-21 10:33:17 -0700

    base of 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'print numbers of base of 10'
+  *Return: always 0 means good
+  */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p <= 9; p++)
+	{
+		printf("%d", p);
+	}
+	printf("\n");
+	return (0);
+}
