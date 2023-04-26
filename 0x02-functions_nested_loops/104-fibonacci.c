f343ff02971f7ada234b97705a15098942932150
BraveStarShalom <jesway1030@gmail.com>
2023-04-25 22:26:46 -0700

    98 fabonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,36 +8,36 @@
 int main(void)
 {
 	unsigned long int p;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
+	unsigned long int str = 1;
+	unsigned long int end = 2;
 	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	unsigned long int str1;
+	unsigned long int str2;
+	unsigned long int end1;
+	unsigned long int end2;
 
-	printf("%lu", bef);
+	printf("%lu", str);
 
 	for (p = 1; p < 91; p++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		printf(", %lu", end);
+		end += str;
+		str = end - str;
 	}
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+	str1 = (str / l);
+	str2 = (str % l);
+	end1 = (end / l);
+	end2 = (end % l);
 
 	for (p = 92; p < 99; ++p)
 	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		printf(", %lu", end1 + (end2 / l));
+		printf("%lu", end2 % l);
+		end1 = end1 + str1;
+		str1 = end1 - str1;
+		end2 = end2 + str2;
+		str2 = end2 - str2;
 	}
 	printf("\n");
 	return (0);
