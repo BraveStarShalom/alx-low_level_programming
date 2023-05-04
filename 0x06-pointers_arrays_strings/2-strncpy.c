76afe0ae0106b160b0bc127ff5493ad396b51bab
BraveStarShalom <jesway1030@gmail.com>
2023-05-04 06:32:15 -0700

    string n copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int p;
 
-	for (p = 0; p < n && src[p] != '\0; p++)
+	for (p = 0; p < n && src[p] != '\0'; p++)
 		dest[p] = src[p];
-
 	for (; p < n; p++)
 		dest[p] = '\0';
 
