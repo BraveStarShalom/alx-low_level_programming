fc473b32a9d5aef30e36e2feec4e69244f71afd4
BraveStarShalom <jesway1030@gmail.com>
2023-05-10 01:59:15 -0700

    recursion main file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int the_pal(char *s, int p, int l);
+int _strlen_recursion(char *s);
+
+/**
+  * is_palindrome - checks if a string is a palindrome
+  * @s: string
+  * Return: 1 if it is a palindrome, 0 if it is not.
+  */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (the_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+  * _strlen_recursion - returns the length of a string
+  * @s: string for length calculation
+  * Return: length of the string.
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+  * the_pal - recursive check for palindrome
+  * @s: string to check
+  * @p: iterator
+  * @l: length of the string
+  * Return: 1 if it is a palindrome, 0 if it is not.
+  */
+int the_pal(char *s, int p, int l)
+{
+	if (*(s + 1) != *(s + l - 1))
+		return (0);
+	if (p >= l)
+		return (1);
+	return (the_pal(s, p + 1, l - 1));
+}
