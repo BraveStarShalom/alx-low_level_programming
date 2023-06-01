5ca48401a26102640ff5b3c12d2e97b37acec7dc
BraveStarShalom <jesway1030@gmail.com>
2023-06-01 13:07:53 -0700

    singly linked task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,15 @@ size_t print_list(const list_t *h)
 {
 	size_t peace = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		peace++;
 		h = h->next;
+		peace++;
 	}
 
 	return (peace);
